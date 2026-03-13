2f68352f7db53d16dc9d8474d000e7e34da595d9
Taibullah789 <taib.khan323@gmail.com>
2026-03-13 21:55:06 +0500

    new

--- src/components/FloatingButtons.jsx
+++ src/components/FloatingButtons.jsx
@@ -6,14 +6,14 @@ const FloatingButtons = () => {
     <>
       {/* Call Button - Left */}
       <a
-  href="tel:+0501478398"
+  href="tel:+971501478398"
   className="fixed bottom-6 left-6 bg-green-600 text-white w-14 h-14 flex items-center justify-center rounded-full shadow-lg z-50"
 >
   <FaPhoneAlt size={20} />
 </a>
 
 <a
-  href="https://wa.me/0501478398"
+  href="https://wa.me/971501478398"
   className="fixed bottom-6 right-6 bg-green-500 text-white w-14 h-14 flex items-center justify-center rounded-full shadow-lg z-50"
 >
   <FaWhatsapp size={22} />
